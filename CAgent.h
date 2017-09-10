0ce21821eb419e7d0f28644b5ff6b451f3ebfd3e
Nikolaj Mariager <nikolaj.mariager@gmail.com>
2017-09-10 21:54:39 +0200

    Fixed virtual function tabel on CAgent.

--- CAgent.h
+++ CAgent.h
@@ -1,6 +1,6 @@
-
-
-
+#include "CAgent_VTable.h"
+#include "Agent/AgentWar3/War3Image/CWidget_VTable.h"
+#include "Agent/AgentWar3/War3Image/Widget/Selectable/CUnit_VTable.h"
 
 /*
 Class ID: +aga
@@ -8,7 +8,11 @@ Class Size: 32
 */
 class CAgent {
 public:
-    int field_0;
+    union {
+        CAgent_VTable* cAgent_vtable;
+        CWidget_VTable* cWidget_vtable;
+        CUnit_VTable* cUnit_vtable;
+      };
     int field_4;
     int field_8;
     int field_C;
