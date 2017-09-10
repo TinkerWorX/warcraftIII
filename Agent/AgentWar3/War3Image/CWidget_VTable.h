0a25a35ad2208f284281966e995e91a6bdb85cff
Nikolaj Mariager <nikolaj.mariager@gmail.com>
2017-09-10 22:01:47 +0200

    Fixed some formatting errors in vtables.

--- Agent/AgentWar3/War3Image/CWidget_VTable.h
+++ Agent/AgentWar3/War3Image/CWidget_VTable.h
@@ -57,4 +57,5 @@ struct CWidget_VTable { /* size = 76 */
     pCWidget__SetLife setLife;
     pCWidget__Function func74;
     pCWidget__Function func75;
-    pCWidget__Function func76;};
+    pCWidget__Function func76;
+};
